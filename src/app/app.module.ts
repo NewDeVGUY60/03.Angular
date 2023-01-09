a8e1a40ca1174fe258ef131ff9d50d3a20cdc81d
NewDeVGUY60 <[guynicolas60@gmail.com](mailto:guynicolas60@gmail.com)>
2023-01-09 14:56:01 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// il faut impoté formsmodule en dessous.
+import { FormsModule } from '@angular/forms';
+
+
 import { AppComponent } from './app.component';
+import { MonComposantComponent } from './mon-composant/mon-composant.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonComposantComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule // add here formsModule for use it later.
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mon-composant/mon-composant.component.ts
@@ -0,0 +1,25 @@
+import { Component,OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mon-composant',
+  templateUrl: './mon-composant.component.html',
+  styleUrls: ['./mon-composant.component.css']
+})
+export class MonComposantComponent implements OnInit {
+
+  hidden:boolean = false;
+
+  ngOnInit(): void {
+  }
+  user = {
+    name : 'Doe',
+    firstName : 'John',
+    age : 25,
+    quote : '',
+    photo : 'https://randomuser.me/api/portraits/lego/2.jpg'
+  };
+  hide(){
+    this.hidden = !this.hidden
+  }
+
+}
